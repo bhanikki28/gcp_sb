41cc12de7d8c9561bd7b04c7ff826d5b94b4c4fb
Poovalingam, Bharathy <Bharathy.Poovalingam@anz.com>
2019-11-15 11:55:24 +1100

    changes

--- CISDemo/src/main/java/com/example/CISDemo/CisDemoApplication.java
+++ CISDemo/src/main/java/com/example/CISDemo/CisDemoApplication.java
@@ -2,11 +2,9 @@ package com.example.CISDemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication(scanBasePackages="com.example")
-@EntityScan(basePackages="com.example")
 @EnableScheduling
 public class CisDemoApplication {
 
--- CISDemo/src/main/java/com/example/dao/TestRepository.java
+++ CISDemo/src/main/java/com/example/dao/TestRepository.java
@@ -3,11 +3,12 @@ package com.example.dao;
 import java.util.List;
 
 import org.springframework.cloud.gcp.data.spanner.repository.SpannerRepository;
+import org.springframework.stereotype.Component;
 
 import com.example.entity.Test;
 import com.google.cloud.spanner.Key;
 
-
+@Component
 public interface TestRepository extends SpannerRepository<Test, Key> {
 	  List<Test> findById(int id);
 }
